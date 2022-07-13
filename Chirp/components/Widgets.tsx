b78d0ed732b09f2ce822c236a30861b396a0807d
Sean Duiser <duzer84@gmail.com>
2022-07-13 11:50:22 -0400

    setting up sanity

---
+++ Chirp/components/Widgets.tsx
@@ -0,0 +1,26 @@
+import { SearchCircleIcon, SearchIcon } from '@heroicons/react/outline'
+import React from 'react'
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
+
+function Widgets() {
+  return (
+    <div className='col-span-2 px-2 mt-2 hidden lg:inline'>
+      {/* Search Box */}
+      <div className='flex items-center space-x-2 bg-gray-100 p-3 rounded-full mt-2'>
+        <SearchIcon className='h-5 w-5 text-gray-400' />
+        <input
+          className='flex-1 outline-none bg-transparent'
+          type='text'
+          placeholder='Search Twitter'
+        />
+      </div>
+      <TwitterTimelineEmbed
+        sourceType='profile'
+        screenName='MiamiDolphins'
+        options={{ height: 1000 }}
+      />
+    </div>
+  )
+}
+
+export default Widgets
---
+++ Chirp/pages/index.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Sidebar from '../components/Sidebar'
+import Feed from '../components/Feed'
+import Widgets from '../components/Widgets'
+
+const Home: NextPage = () => {
+  return (
+    <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
+      <Head>
+        <title>Chirp</title>
+      </Head>
+
+      <main className='grid grid-cols-9'>
+        <Sidebar />
+
+        <Feed />
+
+        <Widgets />
+      </main>
+
+    </div>
+  )
+}
+
+export default Home
