3123d8e385c4042d59e9619c8abf5a1069dafabb
Sean Duiser <duzer84@gmail.com>
2022-07-13 15:48:18 -0400

    working on getting tweets to display

--- Chirp/components/Feed.tsx
+++ Chirp/components/Feed.tsx
@@ -1,15 +1,29 @@
 import { RefreshIcon } from '@heroicons/react/outline'
 import React from 'react'
 import Tweetbox from './Tweetbox'
+import TweetComponent from '../components/Tweet'
+import { Tweet } from '../typings'
 
-function Feed() {
+interface Props {
+  tweets: Tweet[]
+}
+
+function Feed({ tweets }: Props) {
   return (
     <div className='col-span-7 lg:col-span-5 border-x'>
       <div className='flex items-center justify-between'>
         <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
         <RefreshIcon className='h-8 w-8 cursor-pointer text-twitter mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125' />
       </div>
-      <Tweetbox />
+
+      <div>
+        <Tweetbox />
+      </div>
+
+      {/* Feed */}
+      <div>
+        {tweets?.map(tweet =>(<TweetComponent key={tweet._id} tweet={tweet}/>))}
+      </div>
     </div>
   )
 }
--- Chirp/pages/index.tsx
+++ Chirp/pages/index.tsx
@@ -1,11 +1,18 @@
+import type { GetServerSideProps } from 'next'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Sidebar from '../components/Sidebar'
 import Feed from '../components/Feed'
 import Widgets from '../components/Widgets'
+import { fetchTweets } from '../utils/fetchTweets'
+import { Tweet } from '../typings'
 
-const Home: NextPage = () => {
+interface Props {
+  tweets: Tweet[]
+}
+
+const Home = ({ tweets }: Props) => {
+
   return (
     <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
       <Head>
@@ -15,7 +22,7 @@ const Home: NextPage = () => {
       <main className='grid grid-cols-9'>
         <Sidebar />
 
-        <Feed />
+        <Feed tweets={tweets} />
 
         <Widgets />
       </main>
@@ -25,3 +32,14 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tweets = await fetchTweets();
+
+  return {
+    props: {
+      tweets,
+    }
+  }
+}
+
